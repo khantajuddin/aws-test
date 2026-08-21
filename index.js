f90ad82279144e9a5e45d17ebb1f52fa58f2cb45
tajuddinkhan <tajuddin.khan@inxpress.com>
2026-08-21 18:24:26 +0530

    added another endpoint for data

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/data", (req, res) => {
+  res.json({
+    data: "hello workd",
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
